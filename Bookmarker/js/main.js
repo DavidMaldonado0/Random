35c2f9595498057c808452e2a0060cefae5e570c
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2019-04-28 19:02:54 -0600

    JS Schema

--- Bookmarker/js/main.js
+++ Bookmarker/js/main.js
@@ -11,8 +11,37 @@ function saveBookmark(e){
     url: siteURL
   }
   
-  localStorage.setItem('test', 'Hello World');
+  if(localStorage.getItem('bookmarks') === null){
+    var bookmarks = [];
+    bookmarks.push(bookmark);
+    //SET TO LOCALSTORAGE
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    bookmarks.push(bookmark);
+
+    //RESET BACK TO LOCALSTORAGE
+
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
   
   
   e.preventDefault();
+}
+
+
+//FETCH BOOKMARKS
+function fetchBookmarks(){
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  //GET OUTPUT ID 
+  var bookmarksResults = document.getElementById('bookmarksResults');
+  bookmarksResults.innerHTML = '';
+
+  for (var i = 0; i < bookmarks.length; i++ ) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += name;
+  }
 }
